c1e0feb9b6e04db1162327fd7ad5eab5d0832c7c
bob <msabil443@gmail.com>
2025-01-06 06:48:54 +0530

    tower of hanoi problem

--- Recursion/towerofhanoi.cpp
+++ Recursion/towerofhanoi.cpp
@@ -3,10 +3,25 @@
 
 using namespace std;
 
+void tower_of_hanoi (int n, string a = "A", string b = "B", string c="C")
+{
+    if ( n== 1)
+    {
+        cout<<"Move 1 from "<<a<<" to "<<c<<endl;
+        return;
+    }
+    tower_of_hanoi(n-1, a, c, b);
+    cout<<"Move "<<n<<" from "<<a<<" to "<<c<<endl;
+    tower_of_hanoi(n-1, b, a, c);
+}
+
 
 int main ()
 {
-    
-}
+    int n;
+    cin>>n;
+    cout<<"Number of steps:"<<(1<<n)-1<<endl;
+    tower_of_hanoi(n, "A", "B", "C");
+    return 0;
 
-//test
+}
